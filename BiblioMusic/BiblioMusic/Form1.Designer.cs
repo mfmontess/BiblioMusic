4fd61174e99cf8e7caabf866e63111a8aa571d8e
mfmontess <mmontess@ucentral.edu.co>
2017-11-02 16:01:49 -0500

    proyecto inicial

---
+++ BiblioMusic/BiblioMusic/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace BiblioMusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chklstFormatos = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.lstCanciones = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Configuración";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ruta:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chklstFormatos);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnCargar);
+            this.panel1.Controls.Add(this.txtRuta);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(368, 210);
+            this.panel1.TabIndex = 2;
+            // 
+            // chklstFormatos
+            // 
+            this.chklstFormatos.FormattingEnabled = true;
+            this.chklstFormatos.Items.AddRange(new object[] {
+            "mp3",
+            "wav"});
+            this.chklstFormatos.Location = new System.Drawing.Point(78, 97);
+            this.chklstFormatos.Name = "chklstFormatos";
+            this.chklstFormatos.Size = new System.Drawing.Size(120, 34);
+            this.chklstFormatos.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Formatos:";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(140, 175);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 3;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Location = new System.Drawing.Point(78, 60);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(274, 20);
+            this.txtRuta.TabIndex = 2;
+            this.txtRuta.Text = "C:\\Users\\mmontes\\Music";
+            // 
+            // lstCanciones
+            // 
+            this.lstCanciones.FormattingEnabled = true;
+            this.lstCanciones.Location = new System.Drawing.Point(474, 12);
+            this.lstCanciones.Name = "lstCanciones";
+            this.lstCanciones.Size = new System.Drawing.Size(120, 420);
+            this.lstCanciones.TabIndex = 3;
+            this.lstCanciones.SelectedIndexChanged += new System.EventHandler(this.lstCanciones_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 439);
+            this.Controls.Add(this.lstCanciones);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.ListBox lstCanciones;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox chklstFormatos;
+    }
+}
+
---
+++ BiblioMusic/BiblioMusic/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BiblioMusic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            String formatos="";
+            foreach (var chk in chklstFormatos.CheckedItems) {
+                formatos += "."+chk.ToString()+"|";
+            }
+
+            string[] extensions = formatos.Split('|');
+            DirectoryInfo dinfo = new DirectoryInfo(txtRuta.Text);
+            FileInfo[] files = dinfo.GetFiles().Where(f => extensions.Contains(f.Extension.ToLower())).ToArray();
+
+            
+            foreach (var fi in files)
+            {
+                TagLib.File Mp3file = TagLib.File.Create(fi.FullName);
+                lstCanciones.Items.Add(Mp3file);
+                lstCanciones.DisplayMember = "Mp3file.Tag.Title";
+            }
+        }
+
+        private void lstCanciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var curItem = lstCanciones.SelectedItem;
+        }
+    }
+}
